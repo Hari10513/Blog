ab321c7fcd4bb048d9b5c058512b97b72fc3f157
Bharath <Bharath@LAPTOP-IS8K54U6>
2021-12-16 16:22:25 +0530

    Add project files.

---
+++ Blog/Models/AppDbContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Blog.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Blog.Models
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<PostViewModel> Post { get; set; }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server = LAPTOP-IS8K54U6\\SQLSERVER2019; Database=BlogDB; Trusted_Connection=True;");
+            }
+        }*/
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("NAME")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Slug)
+                    .HasColumnName("SLUG")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<PostViewModel>(entity =>
+            {
+                entity.ToTable("Post1", "dbo");
+                entity.HasKey(e => e.PostId);
+                entity.Property(e => e.PostId).HasColumnName("POST_ID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CATEGORY_ID");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("CREATED_DATE")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("DESCRIPTION")
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .HasColumnName("TITLE")
+                    .HasMaxLength(2000)
+                    .IsUnicode(false);
+
+               /*entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Post)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK__Post__CATEGORY_I__267ABA7A");*/
+            });
+        }
+    }
+}
